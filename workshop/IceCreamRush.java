6a1fa252aed60e52013104b1044aa2bd06222aeb
Tarun-pandit <117818613+Tarun-pandit@users.noreply.github.com>
2025-07-22 10:31:22 +0530

    Add files via upload

---
+++ workshop/IceCreamRush.java
@@ -0,0 +1,66 @@
+package Workshop;
+
+public class IceCreamRush {
+
+    static class Flavor {
+        String name;
+        int sales;
+
+        Flavor(String name, int sales) {
+            this.name = name;
+            this.sales = sales;
+        }
+    }
+
+    
+    public static void bubbleSort(Flavor[] flavors) {
+        int n = flavors.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (flavors[j].sales < flavors[j + 1].sales) {
+                    Flavor temp = flavors[j];
+                    flavors[j] = flavors[j + 1];
+                    flavors[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        Flavor[] flavors = {
+            new Flavor("Vanilla", 0),
+            new Flavor("Chocolate", 0),
+            new Flavor("Strawberry", 0),
+            new Flavor("Mint", 0),
+            new Flavor("Cookie Dough", 0)
+        };
+
+        System.out.println("Please enter sales for each flavor:");
+
+        for (Flavor f : flavors) {
+            System.out.print(f.name + ": ");
+            f.sales = scanner.nextInt();
+        }
+
+        System.out.println("\nBefore sorting:");
+        for (Flavor f : flavors) {
+            System.out.println(f.name + ": " + f.sales);
+        }
+
+        bubbleSort(flavors);
+
+        System.out.println("\nAfter sorting by popularity (sales):");
+        for (Flavor f : flavors) {
+            System.out.println(f.name + ": " + f.sales);
+        }
+
+        scanner.close();
+    }
+}
